c27bf28a263f65125946b95e482a013d1dafbcee
Rohan Batra <rohanbatra24@gmail.com>
2021-05-29 12:31:44 -0400

    add togglePlay and showPlayIcon functions

--- script.js
+++ script.js
@@ -5,13 +5,26 @@ const playBtn = document.getElementById("play-btn");
 const volumrIcon = document.getElementById("volume-icon");
 
 // Play & Pause ---- ------------------------------- //
+
+const showPlayIcon = (params) => {
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+};
+
 const togglePlay = (params) => {
   if (video.paused) {
     video.play();
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
   } else {
     video.pause();
+    showPlayIcon();
   }
 };
+
+//  on video end, show play btn icon
+
+video.addEventListener("ended", showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
