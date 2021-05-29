9007cd482adcf3750ea037bd0c80565b75c99f66
Rohan Batra <rohanbatra24@gmail.com>
2021-05-29 12:27:15 -0400

    add play/pause functionality

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+const video = document.querySelector("video");
+const progressRange = document.querySelector(".progress-range");
+const progressBar = document.querySelector(".progress-bar");
+const playBtn = document.getElementById("play-btn");
+const volumrIcon = document.getElementById("volume-icon");
 
-
-// Play & Pause ----------------------------------- //
-
-
+// Play & Pause ---- ------------------------------- //
+const togglePlay = (params) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
 
 // Progress Bar ---------------------------------- //
 
-
-
 // Volume Controls --------------------------- //
-
-
 
 // Change Playback Speed -------------------- //
 
-
-
 // Fullscreen ------------------------------- //
 
-
+// event listners
+playBtn.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
